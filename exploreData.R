0ce4859d9029d33b5f512524d2b07cd6c53c1671
Tom Bailey <bailey@ficonsulting.com>
2017-01-09 23:21:52 -0500

    created piecewise linear model and non-parametric smooth curves

--- exploreData.R
+++ exploreData.R
@@ -1,5 +1,7 @@
 setwd("C:/MyGitRepos/cherry-blossom-run/Data")
 
+load("cbMen.rda")
+
 library(RColorBrewer)
 
 #List functions available in the package
@@ -50,7 +52,44 @@ resid.lo.pr <- predict(resid.lo, newdata = data.frame(age = age20to80))
 lines(x = age20to80, y = resid.lo.pr, col = "green", lwd = 3, lty = 2)
 
 
-###Try piecewise linear model by taking local weighted averages of time as age varies
+###Create non-parametric smooth curve by taking local weighted averages of time as age varies
 #use loess function
 menRes.lo <- loess(runTime ~ age, cbMenSub)
 menRes.lo.pr <- predict(menRes.lo, data.frame(age = age20to80))
+
+###Try piecewise linear model
+#first split at just 50 years old -- y = a + b(age) + c(years over 50)
+over50 <- pmax(0, cbMenSub$age - 50)
+lmOver50 <- lm(runTime ~ age + over50, data = cbMenSub)
+summary(lmOver50)
+
+#create over30, over40, etc. variables
+decades <- seq(30, 60, by = 10)
+overAge <- lapply(decades, function(x) pmax(0, (cbMenSub$age - x)))
+names(overAge) <- paste("over", decades, sep = "")
+overAge <- as.data.frame(overAge)
+
+#create model by regressing on age and all the category (ie over30, over40, etc) variables
+lmPiecewise <- lm(runTime ~ ., 
+                  data = cbind(cbMenSub[, c("runTime", "age")], overAge))
+
+summary(lmPiecewise)
+
+#create predicted values for piecewise function
+overAge20 <- lapply(decades, function(x) pmax(0, (age20to80 - x)))
+names(overAge20) <- paste("over", decades, sep = "")
+overAgeDF <- cbind(data.frame(age = age20to80), overAge20)
+
+predPiecewise <- predict(lmPiecewise, overAgeDF)
+
+###Plot loess curve and piecewise linear model
+plot(predPiecewise ~ age20to80,
+     type = "l", col = "purple", lwd = 3,
+     xlab = "Age (years)", ylab = "Run Time Prediction")
+
+lines(x = age20to80, y = menRes.lo.pr, 
+      col = "green", lty = 2, lwd = 3)
+
+legend("topleft", col = c("purple", "green"),
+       lty = c(1, 2), lwd = 3,
+       legend = c("Piecewise Linear", "Loess Curve"), bty = "n")
