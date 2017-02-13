c70c1056378d32edee7a14c7749041beb0891307
Tom Bailey <bailey@ficonsulting.com>
2017-02-12 22:26:26 -0500

    clean hometown var, create unique ID var

--- indivLongAnalysis.R
+++ indivLongAnalysis.R
@@ -11,6 +11,7 @@ trimBlanks <- function(charVector){
     nameClean <- gsub("[[:blank:]]+", " ", nameClean)
 }
 
+
 nameClean <- trimBlanks(cbMenSub$name)
 
 length(nameClean)
@@ -34,3 +35,38 @@ nameClean <- gsub("[,.]", "", nameClean)
 tabNameYr <- table(cbMenSub$year, nameClean)
 
 max(tabNameYr)
+
+which(tabNameYr == max(tabNameYr))
+indMax <- which(tabNameYr == max(tabNameYr), arr.ind = T)
+
+colnames(tabNameYr)[indMax[2]]
+
+###Add cleaned version of name to df
+cbMenSub$nameClean <- nameClean
+
+###Calculate year of birth for each runner by subtracting age from race year
+cbMenSub$yob <- cbMenSub$year - cbMenSub$age
+
+###Clean home variable and add to cbMenSub df
+homeClean <- trimBlanks(cbMenSub$home)
+homeClean <- tolower(homeClean)
+homeClean <- gsub("[,.`]", "", homeClean)
+cbMenSub$homeClean <- homeClean
+
+###Look at values for Michael Brown, whose name appears 5 times in 2012
+vars <- c("year", "homeClean", "nameClean", "yob", "runTime")
+mb <- which(nameClean == "michael brown")
+birthOrder <- order(cbMenSub$yob[mb])
+cbMenSub[mb[birthOrder], vars]
+
+###Create identifier for individuals
+cbMenSub$ID <- paste(nameClean, cbMenSub$yob, sep="_")
+
+###use tapply to determine how many times each ID appears
+#tapply(Summary Var, Group Var, Function)
+races <- tapply(cbMenSub$year, cbMenSub$ID, length)
+races8 <- names(races)[which(races>=8)]
+men8 <- cbMenSub[cbMenSub$ID %in% races8, ] 
+orderByRunner <- order(men8$ID, men8$year)
+men8 <- men8[orderByRunner, ]
+
