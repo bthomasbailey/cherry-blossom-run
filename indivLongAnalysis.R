323b10931082ebe9a9f1d1a098acff6cbbeefc3b
Tom Bailey <bailey@ficonsulting.com>
2017-02-26 22:28:45 -0500

    finished longitudinal analysis, read in txt files using read.fwf, added gitignore file

--- indivLongAnalysis.R
+++ indivLongAnalysis.R
@@ -70,3 +70,97 @@ men8 <- cbMenSub[cbMenSub$ID %in% races8, ]
 orderByRunner <- order(men8$ID, men8$year)
 men8 <- men8[orderByRunner, ]
 
+###alternative orginization: store them as a list with an element for each ID in races8
+men8L <- split(men8, men8$ID)
+names(men8L) <- races8
+
+###find records where performance varies by more than 20 minutes from year to year
+gapTime <- tapply(men8$runTime, men8$ID, function(t) any(abs(diff(t))) > 20)
+gapTime <- sapply(men8L, function(df) any(abs(diff(df$runTime)) > 20))
+#How many runners are there like this?
+sum(gapTime)
+#show more data on first two of these runners
+lapply(men8L[gapTime][1:2], function(df) df[, vars])
+
+###determine state (or province/country) of each runner and add that to ID variable
+homeLen <- nchar(cbMenSub$homeClean)
+cbMenSub$state <- substr(cbMenSub$homeClean, start = homeLen - 1, stop = homeLen)
+cbMenSub$state[cbMenSub$year == 2006] <- NA
+cbMenSub$ID <- paste(cbMenSub$nameClean, cbMenSub$yob, cbMenSub$state, sep="_")
+
+###select those IDs that occur at least 8 times
+numRaces <- tapply(cbMenSub$year, cbMenSub$ID, length)
+races8 <- names(numRaces)[which(numRaces>=8)]
+men8 <- cbMenSub[cbMenSub$ID %in% races8, ] 
+orderByRunner <- order(men8$ID, men8$year)
+men8 <- men8[orderByRunner, ]
+
+men8L <- split(men8, men8$ID)
+names(men8L) <- races8
+length(races8)
+
+
+###Divide runners up into 9 groups
+groups <- 1 + (1:length(men8L) %% 9)
+
+
+addRunners <- function(listRunners, colors, numLty){
+  numRunners <- length(listRunners)
+  colIndx <- 1 + (1:numRunners) %% length(colors)
+  ltys <- rep(1:numLty, each = length(colors), length = numRunners)
+  
+  mapply(function(df, i){
+          lines(df$runTime ~ df$age,
+                col = colors[colIndx[i]], lwd = 2, lty = ltys[i])    
+        }, listRunners, i = 1:numRunners)
+}
+
+
+colors <- c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", 
+            "#ff7f00", "#a65628")
+
+par(mfrow = c(3, 3), mar = c(2, 2, 1, 1))
+
+invisible(
+sapply(1:9, function(grpId){
+  plot(x = 40, y = 60, type = "n",
+       xlim = c(20, 80), ylim = c(50, 130),
+       xlab = "Age(years)", ylab = "Run Time (minutes)")
+  
+  addRunners(men8L[groups == grpId], colors, numLty = 6)
+}))
+
+
+fitOne <- function(oneRunner, addLine = F, col = "grey"){
+  lmOne <- lm(runTime ~ age, data = oneRunner)
+  if(addLine){
+    lines(x = oneRunner$age, y = predict(lmOne),
+          col = col, lwd = 2, lty = 2)
+  }  
+  
+  ind <- floor((nrow(oneRunner) + 1) / 2)
+  res <- c(coefficients(lmOne)[2], oneRunner$age[ind], 
+           predict(lmOne)[ind])
+  names(res) <- c("ageCoeff", "medAge", "predRunTime")  
+  return(res)
+}
+
+#look at just the 9th group to test out fitOne function
+plot(x = 40, y = 60, type = "n",
+     xlim = c(20, 80), ylim = c(50, 130),
+     xlab = "Age(years)", ylab = "Run Time (minutes)")
+
+addRunners(men8L[groups == 9], colors, numLty = 6)
+lapply(men8L[groups == 9], fitOne, addLine = T, col = "black")
+
+#fit lines for all 306 runners
+men8LongFit <- lapply(men8L, fitOne)
+coeffs <- sapply(men8LongFit, "[", "ageCoeff")
+ages <- sapply(men8LongFit, "[", "medAge")
+
+#Now regress coeffs on age. Is there positive relationship?
+longCoeffs <- lm(coeffs ~ ages)
+summary(longCoeffs)
+
+
+
---
+++ readTxt_using_readfwf.R
@@ -0,0 +1,144 @@
+###Exercise #1: Redo reading in txt files, but do so with read.fwf
+
+setwd("C:/MyGitRepos/cherry-blossom-run/Data")
+
+
+read_txt_fwf <- function(M = T, yr, widths, 
+                         colNames = c("name", "age", "home", "gun", "net"), 
+                         skipRows){
+  
+  dirName <- ifelse(M, "MenTxt", "WomenTxt")
+  file <- file.path(dirName, paste0(yr, ".txt")) 
+  
+  df <- read.fwf(file, widths = widths, col.names = colNames, 
+                 skip = skipRows, comment.char = "")
+  
+  #add column to keep track of year
+  df$year <- yr
+  
+  #keep only one time column - first look for net, then gun, then time
+  useTime <- if("net" %in% colNames) {
+    "net"
+  } else if("gun" %in% colNames) {
+    "gun"
+  } else {
+    "time"}
+  
+  df <- df[, c("year", "name", "age", "home", useTime)]
+  names(df)[names(df) == "net"] <- "time"
+  
+  df$name <- as.character(df$name)
+  df$age <- as.integer(df$age)
+  df$home <- as.character(df$home)
+  df$time <- as.character(df$time)
+  
+  return(df)
+  
+}
+
+
+menWidths <- list(c(-16, 22, 3, 19, 8),
+                  c(-22, 22, 3, 19, 9, 8),
+                  c(-12, 22, 3, 19, 8, 8),
+                  c(-12, 22, 3, 19, 8, 8),               
+                  c(-22, 30, 3, 20, 9, 8),
+                  c(-22, 30, 3, 20, 8, 8),
+                  c(-16, 23, 3, 19, 8, 8), 
+                  c(-22, 23, 3, 24, 9, 6), 
+                  c(-25, 23, 3, 19, 9), 
+                  c(-25, 23, 3, 19, -28, 8),
+                  c(-25, 23, 3, 21, 9, 9), 
+                  c(-25, 23, 3, 21, -8, 8, 9), 
+                  c(-25, 23, 3, 21, -8, 8, 8), 
+                  c(-25, 23, 3, 21, -8, 8)
+                )
+
+menColNames <- list(c("name", "age", "home", "time"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "time"),
+                    c("name", "age", "home", "time"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "time")
+)
+
+
+menSkipRows <- list(3, 2, 5, 3, 3, 8, 8, 9, 7, 8, 8, 8, 8, 8)
+
+names(menWidths) <- 1999:2012
+names(menColNames) <- 1999:2012
+names(menSkipRows) <- 1999:2012
+
+
+womenWidths <- list(c(-16, 22, 3, 19, 8),
+                    c(-22, 22, 3, 19, 9, 8),
+                    c(-12, 22, 3, 19, 8, 8),
+                    c(-12, 22, 3, 19, 8, 8),               
+                    c(-22, 30, 3, 20, 9, 8),
+                    c(-22, 30, 3, 20, 8, 8),
+                    c(-16, 23, 3, 19, 8, 8), 
+                    c(-22, 23, 3, 24, 9, 6), 
+                    c(-25, 23, 3, 19, 9), 
+                    c(-25, 23, 3, 19, -28, 8),
+                    c(-25, 23, 3, 21, 8, 9), 
+                    c(-25, 23, 3, 21, -8, 8, 9), 
+                    c(-25, 23, 3, 21, -8, 8, 8), 
+                    c(-25, 23, 3, 21, -8, 8)
+)
+
+
+womenColNames <- list(c("name", "age", "home", "time"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "net", "gun"),
+                    c("name", "age", "home", "time"),
+                    c("name", "age", "home", "time"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "gun", "net"),
+                    c("name", "age", "home", "time")
+)
+
+
+womenSkipRows <- list(3, 2, 5, 3, 3, 8, 8, 9, 7, 8, 8, 8, 8, 8)
+
+names(womenWidths) <- 1999:2012
+names(womenColNames) <- 1999:2012
+names(womenSkipRows) <- 1999:2012
+
+
+
+#must use SIMPLIFY = F to return list of dataframes; 
+#if not, it will try to reduce the result to a matrix
+menDF_fwf <- mapply(read_txt_fwf, M = T, yr = 1999:2012, widths = menWidths,
+                colNames = menColNames, skipRows = menSkipRows,
+                SIMPLIFY = F)
+
+womenDF_fwf <- mapply(read_txt_fwf, M = F, yr = 1999:2012, widths = womenWidths,
+                      colNames = womenColNames, skipRows = womenSkipRows,
+                      SIMPLIFY = F)
+
+
+#combine each DF in list into one big DF
+cbMen_fwf <- do.call(rbind, menDF_fwf)
+cbWomen_fwf <- do.call(rbind, womenDF_fwf)
+
+#save DFs
+save(cbMen_fwf, file = "cbMen_fwf.rda")
+save(cbWomen_fwf, file = "cbWomen_fwf.rda")
+
+
+
+
+
